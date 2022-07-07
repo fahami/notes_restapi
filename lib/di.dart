518ce2918a9897071e0312d6f7dceb14d349c5d9
Fahmi <fahmi@ekrut.com>
2022-07-07 21:35:38 +0700

    auth local datasource and add service locator dependecy

--- lib/di.dart
+++ lib/di.dart
@@ -3,7 +3,15 @@ import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:notes_restapi/core/network/network_info.dart';
+import 'package:notes_restapi/features/auth/data/datasources/auth_local_datasource.dart';
+import 'package:notes_restapi/features/auth/data/datasources/auth_remote_datasource.dart';
 import 'package:notes_restapi/features/auth/data/model/user_model.dart';
+import 'package:notes_restapi/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:notes_restapi/features/auth/domain/repositories/auth_repository.dart';
+import 'package:notes_restapi/features/auth/domain/usecases/get_user.dart';
+import 'package:notes_restapi/features/auth/domain/usecases/signin.dart';
+import 'package:notes_restapi/features/auth/domain/usecases/signout.dart';
+import 'package:notes_restapi/features/auth/domain/usecases/signup.dart';
 import 'package:notes_restapi/features/todo/data/datasources/color_local_datasource.dart';
 import 'package:notes_restapi/features/todo/data/datasources/color_remote_datasource.dart';
 import 'package:notes_restapi/features/todo/data/datasources/todo_local_datasource.dart';
@@ -46,14 +54,20 @@ Future<void> init() async {
         updateTodo: sl(),
       ));
 
-  // usecases
+  // todo usecases
   sl.registerLazySingleton(() => GetTodos(sl()));
   sl.registerLazySingleton(() => DeleteTodo(sl()));
   sl.registerLazySingleton(() => DeleteAllTodo(sl()));
-  sl.registerLazySingleton(() => GetTodoColor(sl()));
   sl.registerLazySingleton(() => GetTodoById(sl()));
   sl.registerLazySingleton(() => AddTodo(sl()));
   sl.registerLazySingleton(() => UpdateTodo(sl()));
+  // color usecase
+  sl.registerLazySingleton(() => GetTodoColor(sl()));
+  // auth usecase
+  sl.registerLazySingleton(() => GetUserUseCase(sl()));
+  sl.registerLazySingleton(() => SignInUseCase(sl()));
+  sl.registerLazySingleton(() => SignUpUseCase(sl()));
+  sl.registerLazySingleton(() => SignOutUseCase(sl()));
 
   // repository
   sl.registerLazySingleton<TodoRepository>(
@@ -70,6 +84,13 @@ Future<void> init() async {
       networkInfo: sl(),
     ),
   );
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      localDataSource: sl(),
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
 
   // data sources
   sl.registerLazySingleton<TodoLocalDataSource>(
@@ -80,9 +101,13 @@ Future<void> init() async {
       () => ColorRemoteDataSourceImpl(sl()));
   sl.registerLazySingleton<ColorLocalDataSource>(
       () => ColorLocalDataSourceImpl(sl()));
+  sl.registerLazySingleton<AuthLocalDataSource>(
+      () => AuthLocalDataSourceImpl(sl()));
+  sl.registerLazySingleton<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(sl(), sl()));
 
   // other
-  final Dio firestore = Dio();
+  final Dio httpClient = Dio();
   await Hive.initFlutter();
   Hive.registerAdapter(TodoAdapter());
   Hive.registerAdapter(ColorAdapter());
@@ -91,7 +116,7 @@ Future<void> init() async {
   final Box<UserModel> userBox = await Hive.openBox<UserModel>('user');
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
   sl.registerLazySingleton(() => InternetConnectionChecker());
-  sl.registerLazySingleton(() => firestore);
+  sl.registerLazySingleton(() => httpClient);
   sl.registerLazySingleton(() => todoBox);
   sl.registerLazySingleton(() => colorBox);
   sl.registerLazySingleton(() => userBox);
